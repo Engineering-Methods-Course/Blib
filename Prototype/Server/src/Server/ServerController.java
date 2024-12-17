5320f72a151d9c206deb709af47a8b8ff8522b31
GiladBoudman <budithebudman@gmail.com>
2024-12-17 19:40:39 +0200

    added methods editSubscriberDetails getSubscribersList

--- Prototype/Server/src/Server/ServerController.java
+++ Prototype/Server/src/Server/ServerController.java
@@ -48,9 +48,12 @@ public class ServerController extends AbstractServer{
                         sendToAllClients(subscriberDetails); // change to send to client
                         break;
                     case 203:
-
+                        ArrayList<String> editedDetails = editSubscriberDetails((String)command.getMessageContent());
+                        sendToAllClients(editedDetails); // change to send to client
                         break;
                     case 205:
+                        ArrayList<ArrayList<String>> subscribersList = getSubscribersList();
+                        sendToAllClients(subscribersList); // change to send to client
 
                         break;
                     default:
@@ -107,6 +110,15 @@ public class ServerController extends AbstractServer{
         }
     }
 
+
+
+
+
+
+
+
+
+
     /**
      * This method displays the details of a specific subscriber
      * @param messageContent
@@ -138,5 +150,93 @@ public class ServerController extends AbstractServer{
             e.printStackTrace();
         }
         return subscriberDetails;
+    }
+
+    /**
+     * This method edits the details of a specific subscriber
+     * @param messageContent
+     */
+    private ArrayList<String> editSubscriberDetails(String messageContent) {
+        ArrayList<String> editedDetails = new ArrayList<>();
+        try {
+            /**
+             * The message content is split into an array of strings using the regex pattern
+             */
+            String regex = "[,\\.\\s]";
+            String[] details = messageContent.split(regex);
+            String subscriberId = details[0];
+            String subscriberName = details[1];
+            String subscriberPhoneNumber = details[2];
+            String subscriberEmail = details[3];
+
+            /**
+             * The query updates the name, phone number, and email of the subscriber where the id matches the given value
+             */
+            String query = "UPDATE subscriber SET subscriber_name = ?, subscriber_phone_number = ?, subscriber_email = ? WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, subscriberName);
+            statement.setString(2, subscriberPhoneNumber);
+            statement.setString(3, subscriberEmail);
+            statement.setString(4, subscriberId);
+
+            /**
+             *  Execute the update
+             *  If the update was successful, retrieve the updated details
+             */
+            try
+            {
+                statement.executeUpdate();
+                query = "SELECT * FROM subscriber WHERE id = ?";
+                statement = conn.prepareStatement(query);
+                statement.setString(1, subscriberId);
+                ResultSet rs = statement.executeQuery();
+                if (rs.next()) {
+                    editedDetails.add(rs.getString("subscriber_id"));
+                    editedDetails.add(rs.getString("subscriber_name"));
+                    editedDetails.add(rs.getString("subscriber_phone_number"));
+                    editedDetails.add(rs.getString("subscriber_email"));
+                    editedDetails.add(rs.getString("subscriber_status"));
+                }
+            } catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return editedDetails;
+    }
+
+    /**
+     * This method retrieves a list of all subscribers
+     */
+    private ArrayList<ArrayList<String>> getSubscribersList() {
+        ArrayList<ArrayList<String>> subscribersList = new ArrayList<>();
+        try {
+            /**
+             * The query selects all columns from the subscriber table
+             */
+            String query = "SELECT * FROM subscriber";
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            /**
+             * Execute the query
+             * If the query was successful, add the values of the columns to a list
+             * Add the list to the subscribers list
+             */
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                ArrayList<String> subscriberDetails = new ArrayList<>();
+                subscriberDetails.add(rs.getString("subscriber_id"));
+                subscriberDetails.add(rs.getString("subscriber_name"));
+                subscriberDetails.add(rs.getString("subscriber_phone_number"));
+                subscriberDetails.add(rs.getString("subscriber_email"));
+                subscriberDetails.add(rs.getString("subscriber_status"));
+                subscribersList.add(subscriberDetails);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return subscribersList;
     }
 }
