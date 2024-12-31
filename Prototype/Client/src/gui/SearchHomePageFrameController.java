44ae01323cc4cfe241b911323cee8d5e7e68619e
idshay16 <ishay.yulzary@gmail.com>
2024-12-31 15:59:35 +0200

    added the search/homepage UI and controller with some todo notations

---
+++ Prototype/Client/src/gui/SearchHomePageFrameController.java
@@ -0,0 +1,73 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class SearchHomePageFrameController {
+
+    @FXML
+    public Pane menuPane;
+
+    @FXML
+    public Button loginButton;
+
+    @FXML
+    public Button profileButton;
+
+    @FXML
+    public AnchorPane searchPane;
+
+    @FXML
+    public RadioButton nameRadio;
+
+    @FXML
+    public RadioButton genreRadio;
+
+    @FXML
+    public RadioButton descriptionRadio;
+
+    @FXML
+    public TextField searchField;
+
+    @FXML
+    public TextArea descriptionSearch;
+
+    @FXML
+    public Button searchButton;
+
+    public void initialize() {
+        //todo: implement
+    }
+
+    private void searchByName() {
+        //todo: implement
+    }
+
+    private void searchByGenre() {
+        //todo: implement
+    }
+
+    //todo: make sure searchButton moves to layoutY = 500 when option is selected
+    private void searchByDescription() {
+        //todo: implement
+    }
+
+    public void search() {
+        //todo: implement
+        //todo: make sure searchButton moves to layoutY = 500 when option is selected
+        //todo: use radio and either use the private methods of just send it from here
+    }
+
+    public void login() {
+        //todo: implement
+    }
+
+    public void goToProfile() {
+        //todo: implement
+    }
+}
